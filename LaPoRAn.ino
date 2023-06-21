0202bcde2d79cd85c078435822bfa66a6e8047c6
Ignatius Odi <ignodi@gmail.com>
2023-06-21 11:58:30 +0700

    Update LaPoRAn.ino

--- LaPoRAn.ino
+++ LaPoRAn.ino
@@ -1,134 +1,187 @@
-// Library
-#include <EEPROM.h>
+// LIBRARY
 #include <PubSubClient.h>
+#include <Preferences.h>
+#include <SoftwareSerial.h>
 #include <TinyGPSPlus.h>
 #include <WiFi.h>
 
-// Credential
-#include "env/WiFiCredential.h"
+// CREDENTIAL
 #include "env/MQTTCredential.h"
+#include "env/WiFiCredential.h"
 
-// WiFi
-const char* ssid = SSID;
-const char* password = PASSWORD;
+// VARIABLE
+unsigned long prev = 0;
+int counter = 0;
+const int RXpin = 13, TXpin = 14, GPSBaud = 9600;
+// const double latitude = -7.291378;
+// const double longitude = 112.758697;
 
-// MQTT Broker
-const char* mqtt_broker = MQTT_BROKER;
-const char* topic = MQTT_TOPIC;
-const char* mqtt_username = MQTT_USERNAME;
-const char* mqtt_password = MQTT_PASSWORD;
+// MQTT BROKER
+const char *mqtt_broker = MQTT_BROKER;
+const char *mqtt_topic = MQTT_TOPIC;
+const char *mqtt_username = MQTT_USERNAME;
+const char *mqtt_password = MQTT_PASSWORD;
 const int mqtt_port = MQTT_PORT;
 
-// EEPROM
-#define EEPROM_SIZE 512
+// PREFERENCES
+Preferences preferences;
 
-// EEPROM address to store latitude and longitude
-const int eepromAddr = 0;
+// SERIAL CONNECTION TO GPS
+SoftwareSerial ss(RXpin, TXpin);
 
-// GPS Serial Connection
-HardwareSerial SerialGPS(1);  // Use UART 1 for ESP32 (GPIO16: RX, GPIO17: TX)
-TinyGPSPlus gps;              // GPS object
+// TINYGPSPLUS
+TinyGPSPlus gps;
 
-// WiFi and MQTT clients
+// WIFI
+const char *ssid = SSID;
+const char *password = PASSWORD;
+
+// WIFI & MQTT CLIENTS
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
 
-// Connect to WiFi
+// CONNECT TO WIFI
 void connectWiFi() {
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  Serial.println("Connecting to WiFi...");
+  delay(500);
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Not Connected to WiFi!");
+  } else {
+    Serial.println("Connected to WiFi!");
   }
-
-  Serial.println("\nConnected to WiFi!");
 }
 
-// MQTT callback function
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message:");
+// MQTT CALLBACK FUNCTION
+void mqttCallback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("Message: ");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-  Serial.println("-----------------------");
+  Serial.println("---------------------------------");
 }
 
 // Connect to MQTT broker
 void connectMQTT() {
-  mqtt_client.setServer(mqtt_broker, mqtt_port);
-  mqtt_client.setCallback(mqttCallback);
+  // CLIENT ID
+  String client_id = "esp32-client-";
+  client_id += String(WiFi.macAddress());
 
-  while (!mqtt_client.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (mqtt_client.connect("ESP32Client", mqtt_username, mqtt_password)) {
-      Serial.println("Connected to MQTT!");
-    } else {
-      Serial.print("MQTT connection failed, rc=");
-      Serial.print(mqtt_client.state());
-      Serial.println(" Retrying in 1 seconds...");
-      delay(1000);
-    }
+  Serial.print("Attempting MQTT connection...");
+  // ATTEMPT TO CONNECT MQTT
+  if (mqtt_client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+    // SUBSCRIBE MQTT TOPIC
+    mqtt_client.subscribe(mqtt_topic);
+    Serial.println("MQTT connected");
+  } else {
+    Serial.print("failed with state ");
+    Serial.print(mqtt_client.state());
+    delay(3000);
   }
 }
 
-// Publish GPS coordinates to MQTT
-void publishGPS(float latitude, float longitude) {
-  char latStr[10];
-  dtostrf(latitude, 8, 6, latStr);
-
-  char lonStr[10];
-  dtostrf(longitude, 9, 6, lonStr);
-
-  mqtt_client.publish("gps/latitude", latStr);
-  mqtt_client.publish("gps/longitude", lonStr);
+void updateCounter() {
+  preferences.putInt("counter", counter);
 }
 
-// Store GPS coordinates in EEPROM
-void storeGPS(float latitude, float longitude) {
-  EEPROM.put(eepromAddr, latitude);
-  EEPROM.put(eepromAddr + sizeof(float), longitude);
-  EEPROM.commit();
-  Serial.println("GPS coordinates stored in EEPROM!");
+void shiftPreferencesLocation() {
+  for (int i = 0; i < 6; i++) {
+    const char *key = i + "";
+    const char *key2 = i + 2 + "";
+    preferences.putDouble(key, preferences.getDouble(key2));
+  }
+}
+
+void savePreferencesLocation(double value) {
+  const char *key = counter + "";
+  preferences.putDouble(key, value);
 }
 
 void setup() {
-  // Set software serial baud to 115200
+  // SET SOFTWARE SERIAL BAUD TO 115200
   Serial.begin(115200);
-  SerialGPS.begin(9600, SERIAL_8N1, 16, 17);  // GPS module connected to UART1 (GPIO16: RX, GPIO17: TX)
 
+  // SET GPS BAUD
+  ss.begin(GPSBaud);
+
+  // CONNECT WIFI
   connectWiFi();
-  connectMQTT();
+
+  // SET MQTT CALLBACK
+  mqtt_client.setServer(mqtt_broker, mqtt_port);
+  mqtt_client.setCallback(mqttCallback);
+
+  // SET COUNTER
+  preferences.begin("GPS", false);
+  counter = preferences.getInt("counter");
 }
 
 void loop() {
-  while (SerialGPS.available() > 0) {
-    if (gps.encode(SerialGPS.read())) {
-      if (gps.location.isValid()) {
-        float latitude = gps.location.lat();
-        float longitude = gps.location.lng();
+  while (ss.available() > 0) {
+    unsigned long now = millis();
 
-        // Publish GPS coordinates to MQTT
-        publishGPS(latitude, longitude);
-
-        // STORE GPS coordinates to EEPROM
-        storeGPS(latitude, longitude);
-
-        Serial.print("Latitude: ");
-        Serial.println(latitude, 6);
-        Serial.print(", Longitude: ");
-        Serial.println(longitude, 6);
+    // ATTEMPT RECONNECT TO WIFI EVERY 5 SECONDS
+    if (now - prev % 5000 == 0) {
+      if (WiFi.status() != WL_CONNECTED) {
+        WiFi.reconnect();
       }
+    }
+
+    // ATTEMPT GET GPS LOCATION EVERY 30 SECONDS
+    if (now - prev >= 30000) {
+      // GET GPS LOCATION
+      gps.encode(ss.read());
+
+      if (gps.location.isUpdated()) {
+        double latitude = gps.location.lat();
+        double longitude = gps.location.lng();
+
+        // SAVE TO EEPROM
+        preferences.begin("GPS", false);
+        if (counter == 8) {
+          shiftPreferencesLocation();
+          counter -= 2;
+          updateCounter();
+        }
+        savePreferencesLocation(latitude);
+        counter++;
+        updateCounter();
+        savePreferencesLocation(longitude);
+        counter++;
+        updateCounter();
+
+        // IF CONNECTED TO WIFI, DUMP ALL EEPROM TO MQTT
+        if (WiFi.status() == WL_CONNECTED) {
+          // IF MQTT DISCONNECTED, RECONNECT
+          if (!mqtt_client.connected()) {
+            connectMQTT();
+          }
+          // PUBLISH GPS LOCATION TO MQTT TOPIC
+          for (int i = 0; i < counter / 2; i++) {
+            const char *lat = i * 2 + "";
+            String ltt = String(preferences.getDouble(lat), 7);
+            // char ltt[11] = dtostrf(preferences.getDouble(lat), 9, 7, ltt);
+            const char *lng = i * 2 + 1 + "";
+            String lnt = String(preferences.getDouble(lng), 7);
+            // char lnt[11] = dtostrf(preferences.getDouble(lng), 9, 7, lnt);
+
+            String result = ltt + ", " + lnt;
+            mqtt_client.publish(mqtt_topic, result.c_str());
+          }
+          counter = 0;
+          updateCounter();
+        }
+
+        // CLOSE PREFERENCES
+        preferences.end();
+      }
+
+      // RESET PREV
+      prev = now;
     }
   }
 
-  // Reconnect to MQTT if disconnected
-  if (!mqtt_client.connected()) {
-    connectMQTT();
-  }
-
-  // MQTT client loop
+  // MQTT CLIENT LOOP
   mqtt_client.loop();
 }
